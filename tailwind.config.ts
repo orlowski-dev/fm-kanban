24e0f062c2007d9f1bfac45827dd9ce4eb856ca8
orlowski-dev <orlowski.dev@gmail.com>
2024-01-26 10:37:26 +0100

    Initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,64 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      black: "#000112",
+      "very-dark-grey": "#20212C",
+      "dark-grey": "#2B2C37",
+      "lines-dark": "#3E3F4E",
+      "medium-grey": "#828FA3",
+      "lines-light": "#E4EBFA",
+      "light-grey": "#F4F7FD",
+      white: "#ffffff",
+      "main-purple": "#635FC7",
+      "main-purple-hover": "#A8A4FF",
+      red: "#EA5555",
+      "red-hover": "#FF9898",
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontSize: {
+        hxl: [
+          "1.625rem",
+          {
+            lineHeight: "2rem",
+            fontWeight: 700,
+          },
+        ],
+        hlg: [
+          "1.25rem",
+          {
+            lineHeight: "1.5625rem",
+            fontWeight: 700,
+          },
+        ],
+        hmd: [
+          "1.0625rem",
+          {
+            lineHeight: "1.3125rem",
+            fontWeight: 700,
+          },
+        ],
+        hsm: [
+          "0.875rem",
+          {
+            lineHeight: "1.0625rem",
+            fontWeight: 700,
+          },
+        ],
+        body: [
+          "0.9375rem",
+          {
+            lineHeight: "1.5625rem",
+            fontWeight: 500,
+          },
+        ],
+        bodysm: [
+          "0.875rem",
+          {
+            lineHeight: "1.0625rem",
+            fontWeight: 700,
+          },
+        ],
       },
     },
   },
