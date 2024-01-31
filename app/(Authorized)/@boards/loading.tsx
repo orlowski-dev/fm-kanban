22d703775b12e53b1964eb8e80783069e3af4694
orlowski-dev <orlowski.dev@gmail.com>
2024-01-31 19:11:47 +0100

    Updated boards loading indicator

--- app/(Authorized)/@boards/loading.tsx
+++ app/(Authorized)/@boards/loading.tsx
@@ -1,3 +1,21 @@
 export default function BoardsLoading() {
-  return <p>loading boards..</p>;
+  return (
+    <div>
+      <h2 className="text-bodysm font-medium md:font-bold uppercase text-medium-grey px-6 lg:px-8 py-5">
+        All boards
+      </h2>
+      <div className="px-6 py-3 lg:px-8 text-bodysm text-medium-grey flex items-center gap-3 transition-colors;">
+        <div className="w-[1.125rem] h-[1.125rem] bg-medium-grey animate-pulse rounded-md"></div>
+        <div className="w-[100px] h-[1.125rem] bg-medium-grey animate-pulse rounded-md"></div>
+      </div>
+      <div className="px-6 py-3 lg:px-8 text-bodysm text-medium-grey flex items-center gap-3 transition-colors;">
+        <div className="w-[1.125rem] h-[1.125rem] bg-medium-grey animate-pulse rounded-md"></div>
+        <div className="w-[100px] h-[1.125rem] bg-medium-grey animate-pulse rounded-md"></div>
+      </div>
+      <div className="px-6 py-3 lg:px-8 text-bodysm text-medium-grey flex items-center gap-3 transition-colors;">
+        <div className="w-[1.125rem] h-[1.125rem] bg-medium-grey animate-pulse rounded-md"></div>
+        <div className="w-[100px] h-[1.125rem] bg-medium-grey animate-pulse rounded-md"></div>
+      </div>
+    </div>
+  );
 }
